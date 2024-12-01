6250f4828461a9894aa013541584243627b5e7b8
hoaian1377 <tommyjan1000>
2024-12-01 22:21:03 +0700

    New Upload

--- site1/home/urls.py
+++ site1/home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from home import views as home
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home.get_home,name='home')
+    path('',views.home,name='home'),
+    path('badminton_court_booking/',views.badminton_court_booking,name='badminton_court_booking')
+    
 ]
--- site1/home/views.py
+++ site1/home/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def get_home(request):
+def home(request):
     return render(request,'home.html')
+def badminton_court_booking(request):
+    return render(request,'badminton_court_booking.html')
