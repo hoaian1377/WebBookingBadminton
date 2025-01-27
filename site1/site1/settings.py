95128f3f0d859cdcc50b7e4223c2f1084ec34476
NguyenQuangDung0309 <dung007ku@gmail.com>
2025-01-27 15:28:19 +0700

    upload

--- site1/site1/settings.py
+++ site1/site1/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'DLSCLFL',
         'User':'',
         'Password':'',
-        'Host':'MSI', 
+        'Host':'LAPTOP-33IP66FPI', 
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',  # Kiểm tra driver bạn đã cài đặt
             'trust_connection' : 'yes'
