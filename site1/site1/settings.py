e86a1c9e313e670552fd89790ef1b7cc286fd3e3
hoaian1377 <tommyjan1000>
2025-02-19 12:41:14 +0700

    upload Thông báo

--- site1/site1/settings.py
+++ site1/site1/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'site1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'mssql',
-        'NAME': 'QLSCL',  # Database chính
+        'NAME': 'DLBMT',  # Database chính
         'USER': '',  # Nếu dùng SQL Authentication thì điền USER & PASSWORD
         'PASSWORD': '',
-        'HOST': 'DESKTOP-8LHJJ0H',
+        'HOST': 'DESKTOP-AV5VHME',
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
             'trusted_connection': 'yes',  # Nếu dùng Windows Authentication
